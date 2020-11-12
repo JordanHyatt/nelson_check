ff57d6efaf81103e758ec8ef3fd511195ec8a0f7
Jordan Hyatt <jordan.t.hyatt@gmail.com>
2020-11-12 03:46:19 -0500

    getting ready to publish on pypi

--- NelsonCheck/NelsonCheck.py
+++ NelsonCheck/NelsonCheck.py
@@ -1,53 +1,56 @@
-RULES = {
-    1:{
-        'num':1,
-        'desc':'One point is more than 3 standard deviations from the mean',
-        'problem':'One sample is grossly out of control.',
-    },
-    2:{
-        'num':2,
-        'desc':'Nine or more points in a row are on the same side of the mean',
-        'problem':'Some prolonged bias exists'
-    },
-    3:{
-        'num':3,
-        'desc':'Six or more points in a row are continually increasing or decreasing',
-        'problem':'A trend exist',
-    },
-    4:{
-        'num':4,
-        'desc':'Fourteen or more points in a row alternate in direction, increasing then decreasing',
-        'problem':'This much oscillation is beyond noise',
-    },
-    5:{
-        'num':5,
-        'desc':'Two or three out of three points in a row are more than 2 standard deviations from the mean in the same direction',
-        'problem':'Medium tendency for samples to be mediumly out of control',
-    },
-    6:{
-        'num':6,
-        'desc':'Four or five out of five points in a row are more than 1 standard deviation from the mean in the same direction',
-        'problem':'Strong tendency for samples to be slightly out of control',
-    },
-    7:{
-        'num':7,
-        'desc':'Fifteen points in a row are all within 1 standard deviation of the mean on either side of the mean',
-        'problem':'Greater variation would be expected',
-    },
-    8:{
-        'num':8,
-        'desc':'Eight points in a row exist, but none within 1 standard deviation of the mean, and the points are in both directions from the mean',
-        'problem':'Jumping about and below while skipping the 1 sigma band is rarely random',
-    },
-}
+from pandas import DataFrame as DF
+from pandas import Series
+import numpy as np
 
 class NelsonRule:
+    RULES = {
+        1:{
+            'num':1,
+            'desc':'One point is more than 3 standard deviations from the mean',
+            'problem':'One sample is grossly out of control.',
+        },
+        2:{
+            'num':2,
+            'desc':'Nine or more points in a row are on the same side of the mean',
+            'problem':'Some prolonged bias exists'
+        },
+        3:{
+            'num':3,
+            'desc':'Six or more points in a row are continually increasing or decreasing',
+            'problem':'A trend exist',
+        },
+        4:{
+            'num':4,
+            'desc':'Fourteen or more points in a row alternate in direction, increasing then decreasing',
+            'problem':'This much oscillation is beyond noise',
+        },
+        5:{
+            'num':5,
+            'desc':'Two or three out of three points in a row are more than 2 standard deviations from the mean in the same direction',
+            'problem':'Medium tendency for samples to be mediumly out of control',
+        },
+        6:{
+            'num':6,
+            'desc':'Four or five out of five points in a row are more than 1 standard deviation from the mean in the same direction',
+            'problem':'Strong tendency for samples to be slightly out of control',
+        },
+        7:{
+            'num':7,
+            'desc':'Fifteen points in a row are all within 1 standard deviation of the mean on either side of the mean',
+            'problem':'Greater variation would be expected',
+        },
+        8:{
+            'num':8,
+            'desc':'Eight points in a row exist, but none within 1 standard deviation of the mean, and the points are in both directions from the mean',
+            'problem':'Jumping about and below while skipping the 1 sigma band is rarely random',
+        },
+    }
     def __init__(self,num):
         assert type(num)==int, "Rule Number Must Be an Integer Between 1-8"
         assert (num>=1 and num<=8), "Rule Number Must Be an Integer Between 1-8"
         self.num = num
-        self.desc = RULES[self.num]['desc']
-        self.problem = RULES[self.num]['problem']
+        self.desc = self.RULES[self.num]['desc']
+        self.problem = self.RULES[self.num]['problem']
     def __str__(self):
         return f'Nelson Rule {self.num}'
 
@@ -68,10 +71,14 @@ class NelsonViolation:
         
         
 class NelsonCheck:
-    def __init__(self,data,CL,LCL,UCL,exclude_rules=[]):
+    def __init__(self,data,CL=None,LCL=None,UCL=None,exclude_rules=[]):
         self.violations = []#List Of Nelston Violation Objects
         self.result = True # Result after testing rules
-        self.data=data; self.CL=CL; self.LCL=LCL; self.UCL=UCL
+        self.data=Series(data); self.CL=CL; self.LCL=LCL; self.UCL=UCL
+        if self.CL==None: self.CL=np.mean(self.data)
+        std = np.std(self.data)
+        if self.LCL==None: self.LCL=self.CL - 3*std
+        if self.UCL==None: self.UCL=self.CL + 3*std
         
         self.rule_check_dict = {
             1:self.check_rule1,2:self.check_rule2,3:self.check_rule3,
@@ -99,6 +106,7 @@ class NelsonCheck:
         rule = NelsonRule(1)
         mask1 = self.data<self.LCL
         mask2 = self.data>self.UCL
+        
         _r1_offenders = self.data[mask1|mask2]
         return self.process_offenders(rule,_r1_offenders)
 
